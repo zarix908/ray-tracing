9badd99800a75db96f27c839b1a947a29bac6711
zarix908 <daniyarrus@gmail.com>
2021-09-05 22:08:03 +0500

    add vec3

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use std::{fs::File, io::prelude::*, io};
+use std::{fs::File, io, io::prelude::*};
+mod vec3;
+use vec3::Vec3;
 
 fn main() {
-    let img_width = 256;
-    let img_height = 256;
+    let mut a = Vec3::new(1.0, 2.0, 3.0);
+    let mut b = -Vec3::new(1.0, 2.0, 3.0);
+    a += &b;
+    b *= 3.0;
+    b /= 3.0;
+    println!("{:?}", a);
+    println!("{:?}", b);
+    // let img_width = 256;
+    // let img_height = 256;
 
-    let mut img = File::create("output/image.ppm").unwrap();
+    // let mut img = File::create("output/image.ppm").unwrap();
 
-    writeln!(img, "P3\n{} {}\n255", img_width, img_height).unwrap();
+    // writeln!(img, "P3\n{} {}\n255", img_width, img_height).unwrap();
 
-    for j in (0..img_height).rev() {
-        println!("scanlines remaining: {}", j);
-        io::stdout().flush().unwrap();
-        for i in 0..img_width {
-            let r = i as f64 / (img_width - 1) as f64 * 255.999;
-            let g = j as f64 / (img_height - 1) as f64 * 255.999;
-            let b = 0.25 * 255.999;
+    // for j in (0..img_height).rev() {
+    //     println!("scanlines remaining: {}", j);
+    //     io::stdout().flush().unwrap();
+    //     for i in 0..img_width {
+    //         let r = i as f64 / (img_width - 1) as f64 * 255.999;
+    //         let g = j as f64 / (img_height - 1) as f64 * 255.999;
+    //         let b = 0.25 * 255.999;
 
-            writeln!(img, "{} {} {}", r as u8, g as u8, b as u8).unwrap();
-        }
-    }
+    //         writeln!(img, "{} {} {}", r as u8, g as u8, b as u8).unwrap();
+    //     }
+    // }
 }
---
+++ src/vec3.rs
@@ -0,0 +1,60 @@
+use std::ops::{AddAssign, DivAssign, MulAssign, Neg};
+
+#[derive(Debug)]
+pub struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.x
+    }
+    pub fn y(&self) -> f64 {
+        self.y
+    }
+    pub fn z(&self) -> f64 {
+        self.z
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(mut self) -> Vec3 {
+        self.x = -self.x;
+        self.y = -self.y;
+        self.z = -self.z;
+
+        self
+    }
+}
+
+impl AddAssign<&Vec3> for Vec3 {
+    fn add_assign(&mut self, rhs: &Vec3) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        self.x /= rhs;
+        self.y /= rhs;
+        self.z /= rhs;
+    }
+}
