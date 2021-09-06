9079b40744b2f232adcb5aad2547ab859f55217d
zarix908 <daniyarrus@gmail.com>
2021-09-06 11:16:40 +0500

    add normals

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ mod ray;
 use ray::Ray;
 
 fn ray_color(ray: &Ray) -> Vec3 {
-    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        Vec3::new(1.0, 0.0, 0.0)
+    let mut t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let n = ray.at(t) - Vec3::new(0.0, 0.0, -1.0);
+        0.5 * Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0)
     } else {
-        let t = 0.5 * (ray.direction().normalize().y() + 1.0);
+        t = 0.5 * (ray.direction().normalize().y() + 1.0);
         (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
     }
 }
@@ -23,13 +25,18 @@ fn write_color(file: &mut File, color: &Vec3) -> Result<(), io::Error> {
     )
 }
 
-fn hit_sphere(center: Vec3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Vec3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin() - &center;
     let a = ray.direction().dot(ray.direction());
     let b = 2.0 * oc.dot(ray.direction());
     let c = oc.dot(&oc) - radius * radius;
     let disc = b * b - 4.0 * a * c;
-    disc > 0.0
+
+    if disc < 0.0 {
+        -1.0
+    } else {
+        (-b - disc.sqrt()) / (2.0 * a)
+    }
 }
 
 fn main() {
